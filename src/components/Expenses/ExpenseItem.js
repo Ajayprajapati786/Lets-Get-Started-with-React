7edd31579e97ebb99b36a9c761f2a3a9b1e7ace9
Ajay Prajapati <prajapatisani9@gmail.com>
2023-02-12 18:11:26 +0530

    added a button next to each expense which changes expense to 100$ whenever we click on it

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,26 +2,33 @@ import './ExpenseItem.css'
 import './ExpenseDate'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card';
+import React, {useState} from 'react';
 
-const clickHandler =() =>{
-    console.log("Clicked!!!!");
-};
 
-const deleteExpenseHandler = () => {
-    const parent = document.querySelector('.expense-item').parentNode;
-    parent.removeChild(document.querySelector('.expense-item'));
-  };
 
 const ExpenseItem=(props)=>{
+
+    const [title,setTitle] = useState(props.title);
+
+
+const clickHandler =() =>{
+    console.log(title);
+    setTitle("Ajay");
+};
+
+const [amount,setAmount]=useState(props.amount);
+const updateExpense=()=>{
+    setAmount(100);
+}
   return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>${amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
-            <button onClick={deleteExpenseHandler}>Delete Expense</button>
+            <button onClick={updateExpense}>update Expense</button>
         </Card>
     )
 }
