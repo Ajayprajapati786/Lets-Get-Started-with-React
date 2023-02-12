9d50260bd4d9a4e52670a3de28e57849eb91dfa8
Ajay Prajapati <prajapatisani9@gmail.com>
2023-02-12 20:51:12 +0530

    Sharpener Advance javascript task 5 by own

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,16 +1,30 @@
+import { useState } from "react"
 import "./ExpenseForm.css"
 
 const ExpenseForm = () =>{
+    
+    const [enteredtitle, setEnteredtitle ] = useState("123");
+    const titleChangeHandler =(event) =>{
+        console.log(event.target.value)
+        setEnteredtitle(event.target.value);
+    }
+
+    const [enteredAmount, SetenteredAmount] = useState(0.04);
+
+    const amountChangeHandler =(event) =>{
+        console.log(event.target.value)
+        SetenteredAmount(event.target.value);
+    }
   return <form>
   <div className="new-expense__controls">
         <div className="new-expense__control">
             <label>Title</label>
-            <input type="text"/>
+            <input type="text" onChange={titleChangeHandler} value={enteredtitle}/>
         </div>
 
         <div className="new-expense__control">
             <label>Amount</label>
-            <input type="number" min="0.01" step="0.01"/>
+            <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={enteredAmount}/>
         </div>
 
         <div className="new-expense__control">
