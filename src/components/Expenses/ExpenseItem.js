22b3fbacd23d1c7d185e607ecd37c4aa4fc9620b
Ajay Prajapati <prajapatisani9@gmail.com>
2023-02-12 17:12:50 +0530

    added the delete button by which you can delete the expense via DOM manipulation.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,17 +3,25 @@ import './ExpenseDate'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card';
 
+const clickHandler =() =>{
+    console.log("Clicked!!!!");
+};
+
+const deleteExpenseHandler = () => {
+    const parent = document.querySelector('.expense-item').parentNode;
+    parent.removeChild(document.querySelector('.expense-item'));
+  };
+
 const ExpenseItem=(props)=>{
-   const month = props.date.toLocaleString('en-US', {month:'long'});
-   const day = props.date.toLocaleString('en-US', {day:'2-digit'});
-   const year = props.date.getFullYear();
-    return (
+  return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
+            <button onClick={deleteExpenseHandler}>Delete Expense</button>
         </Card>
     )
 }
